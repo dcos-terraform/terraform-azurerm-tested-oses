16ca1f7e6f349c68ccf4961e749627e83c1e4575
Jan Ulferts <julferts@mesosphere.com>
2020-06-12 13:22:30 +0200

    fix: image var type

--- variables.tf
+++ variables.tf
@@ -13,23 +13,18 @@ variable "traditional_default_os_user" {
 
 variable "azure_os_image_version" {
   description = "Azure Images"
-  type        = map(string)
+  type        = map(list(string))
 
   default = {
-    "centos_7.2"      = ["CentOS", "OpenLogic", "7.2", "7.2.20170517"]
-    "centos_7.3"      = ["CentOS", "OpenLogic", "7.3", "7.3.20170707"]
-    "centos_7.5"      = ["CentOS", "OpenLogic", "7.5", "7.5.20180815"]
-    "centos_7.6"      = ["CentOS", "OpenLogic", "7.6", "7.6.20190402"]
-    "centos_7_ci"     = ["CentOS-CI", "OpenLogic", "7-CI", "7.6.20190426"]
-    "coreos_835.13.0" = ["CoreOS", "CoreOS", "Stable", "835.13.0"]
-    "coreos_1235.9.0" = ["CoreOS", "CoreOS", "Stable", "1235.9.0"]
-    "coreos_1855.5.0" = ["CoreOS", "CoreOS", "Stable", "1855.5.0"]
-    "coreos_2079.6.0" = ["CoreOS", "CoreOS", "Stable", "2079.6.0"]
-    "rhel_7.3"        = ["RHEL", "RedHat", "7.3", "7.3.2017053118"]
-    "rhel_7.6"        = ["RHEL", "RedHat", "7.6", "7.6.2019060521"]
-    "rhel_7_ci"       = ["RHEL", "RedHat", "7-RAW-CI", "7.6.2019062019"]
-    "rhel_8"          = ["RHEL", "RedHat", "8", "8.0.2019062020"]
-    "windows_1809"    = ["WindowsServer", "MicrosoftWindowsServer", "Datacenter-Core-1809-with-Containers-smalldisk", "latest"]
+    "centos_7.2"  = ["CentOS", "OpenLogic", "7.2", "7.2.20170517"]
+    "centos_7.3"  = ["CentOS", "OpenLogic", "7.3", "7.3.20170707"]
+    "centos_7.5"  = ["CentOS", "OpenLogic", "7.5", "7.5.20180815"]
+    "centos_7.6"  = ["CentOS", "OpenLogic", "7.6", "7.6.20190402"]
+    "centos_7_ci" = ["CentOS-CI", "OpenLogic", "7-CI", "7.6.20190426"]
+    "rhel_7.3"    = ["RHEL", "RedHat", "7.3", "7.3.2017053118"]
+    "rhel_7.6"    = ["RHEL", "RedHat", "7.6", "7.6.2019060521"]
+    "rhel_7_ci"   = ["RHEL", "RedHat", "7-RAW-CI", "7.6.2019062019"]
+    "rhel_8"      = ["RHEL", "RedHat", "8", "8.0.2019062020"]
   }
 }
 
@@ -37,4 +32,3 @@ variable "os" {
   description = "Operating system to use"
   default     = "centos_7.6"
 }
-
